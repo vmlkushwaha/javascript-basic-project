74386e99f7632344ae1a02ee7da2dab768b2f20f
vimalkwh <vimalkwh05@gmail.com>
2020-08-14 10:48:12 +0530

    complete menu project

--- menu/app.js
+++ menu/app.js
@@ -1,5 +1,4 @@
-const menu = [
-  {
+const menu = [{
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
@@ -71,4 +70,76 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+
+const container = document.querySelector('.btn-container');
+
+//load items
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItem(menu);
+  displayMenuButtons();
+});
+
+
+
+
+function displayMenuItem(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    return `<article class="menu-item">
+      <img src=${item.img} class="photo" alt=${item.title}>
+      <div class="item-info">
+        <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">${item.desc}</p>
+      </div>
+    </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+
+}
+
+function displayMenuButtons() {
+
+  const categories = menu.reduce(function (value, item) {
+    if (!value.includes(item.category)) {
+      value.push(item.category);
+    }
+    return value;
+  }, ['all']);
+
+  const categoryBtns = categories.map(function (category) {
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`
+  }).join('');
+  container.innerHTML = categoryBtns;
+  const filterBtn = document.querySelectorAll('.filter-btn');
+  //filter items
+  filterBtn.forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem
+        }
+      });
+      if (category === 'all') {
+        displayMenuItem(menu);
+      } else {
+        displayMenuItem(menuCategory);
+      }
+    });
+  });
+
+}
